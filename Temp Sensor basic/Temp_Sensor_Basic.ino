47ee95960935765752610334fbef83a377417c2d
Prencious Coder <devansh.panchal03@gmail.com>
2022-02-25 11:13:25 +0530

    Add files via upload

---
+++ Temp Sensor basic/Temp_Sensor_Basic.ino
@@ -0,0 +1,13 @@
+int pinTemp=A1;
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  int temp=analogRead(pinTemp);
+  temp=temp*0.48828125;
+  Serial.print("Temperature:");
+  Serial.print(temp);
+  Serial.println("C");
+  delay(1000);
+}
